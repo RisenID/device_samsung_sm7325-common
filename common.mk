20ac9f29202667245f034d4041e70fae39c83b85
Mathieu Chartier <mathieuc@google.com>
2022-06-01 00:42:41 +0100

    sm7325: Always preopt extracted APKs

--- common.mk
+++ common.mk
@@ -438,6 +438,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/samsung \
     hardware/samsung/aidl/power-libperfmgr
+    
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Settings \
