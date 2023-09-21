0a1ef90624a10807f94e338b903af932fd528c12
Ruchit <risenid@duck.com>
2023-09-21 22:42:03 +0530

    sm7325: R.I.P. SimpleDeviceConfig you really wont be missed much

--- common.mk
+++ common.mk
@@ -456,10 +456,6 @@ TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 # Live Wallpaper
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
-# SimpleDeviceConfig
-PRODUCT_PACKAGES += \
-    SimpleDeviceConfig
-
 #NGA
 PRODUCT_PACKAGES += \
     NgaResources \
